a84615621f21114e514cd3907ed53d82718d52e7
yayxs <leaderywl@163.com>
2020-09-20 19:33:33 +0800

    add vite-app-init

--- vite-app-init/src/components/HelloWorld.vue
+++ vite-app-init/src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
+  <section>
+    <h1>{{ msg }}</h1>
+    <button @click="handleCountAddClick">count is: {{ count }}</button>
+    <hr />
+    <section style="border: 1px solid red; width: 500px; height: 200px">
+      <input type="text" v-model="currIptVal" />
+    </section>
+  </section>
 </template>
 
 <script>
@@ -11,8 +17,21 @@ export default {
   },
   data() {
     return {
+      currIptVal: "currIptVal",
       count: 0,
+      info: {
+        name: "infoName",
+      },
+      listArr: [],
     };
+  },
+  methods: {
+    handleCountAddClick() {
+      // console.log(this);
+      // let { count, msg } = this;
+      // count++;
+      this.count++;
+    },
   },
 };
 </script>
