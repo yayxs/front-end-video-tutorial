1265dcce7f494c3adf4cc72101053e0069cf7c9d
yayxs <leaderywl@163.com>
2020-09-20 14:02:48 +0800

    使用vite初始化Vue3项目

---
+++ vite-app-init/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
