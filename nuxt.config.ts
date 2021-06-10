8f6ce5a0f2e9ea9db4af900738eebfaf1716b49f
yayxs <leaderywl@163.com>
2021-06-10 23:34:43 +0800

    fix(fix interface): fix interface

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+import type { NuxtConfig } from '@nuxt/types'
+
+const config: NuxtConfig = {
+  build: {},
+  buildModules: ['@nuxtjs/composition-api/module', '@nuxt/typescript-build'],
+  css: [],
+  env: {},
+  head: {
+    title: 'nuxt-blog-app',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'A boilerplate to start a Nuxt+TS project quickly',
+      },
+    ],
+    link: [],
+  },
+  loading: { color: '#0c64c1' },
+  modules: [],
+  plugins: ['~/plugins/element-ui'],
+}
+
+export default config
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,152 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">nuxt-ts-app</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <nuxt-link to="/">
+    <el-card
+      v-for="item in itemList"
+      :key="item.id"
+      shadow="hover"
+      class="card-container"
+    >
+      <div class="article-item">
+        <div class="item-info">
+          <div class="info-text">
+            <h3>{{ item.title }}</h3>
+            <div class="text-desc">
+              {{ item.summary || '' }}
+            </div>
+          </div>
+          <div class="info-data">
+            <div class="data-view">
+              <i class="el-icon-view"></i>
+              <span>972</span>
+            </div>
+            <div class="data-view">
+              <i class="el-icon-chat-round"></i>
+              <span>12</span>
+            </div>
+            <div class="data-view">
+              <i class="el-icon-price-tag"></i>
+              <span>{{ item.labelCount || 9 }}</span>
+            </div>
+            <div class="data-view">
+              <span>创建时间</span>
+              <span> {{ item.createAt }} </span>
+            </div>
+            <div class="data-view">
+              <span>作者：</span>
+              <span> vast </span>
+            </div>
+          </div>
+        </div>
+        <div class="item-right-img">
+          <img :src="item.coverImg" alt="" />
+        </div>
       </div>
-    </div>
-  </div>
+    </el-card>
+  </nuxt-link>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import axios from 'axios'
+import { defineComponent, onMounted, ref, Ref } from '@nuxtjs/composition-api'
 
-export default Vue.extend({})
+// interface IArticleItem {
+//   title: string
+//   summary: string
+//   labelCount: number
+//   coverImg: string
+// }
+
+export default defineComponent({
+  setup() {
+    const itemList: Ref<any> = ref([])
+    onMounted(() => {
+      console.log(11)
+      axios('http://101.201.148.180:3009/api/v1/article?offset=0&limit=10')
+        .then((res: any) => {
+          console.log(res.data)
+          if (res.data.code === 0) {
+            console.log('1111111')
+            itemList.value = res.data
+            console.log(itemList.value)
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    })
+    return {
+      itemList,
+    }
+  },
+})
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss" scoped>
+.card-container {
+  .el-card__body {
+    padding: 0;
+    .article-item {
+      padding: 20px;
+      background-color: #fff;
+      border: 1px solid #f0f0f0;
+      overflow: hidden;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      cursor: pointer;
+      .item-info {
+        min-height: 120px;
+        padding-right: 40px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .info-text {
+          h3 {
+            color: #333;
+            margin-bottom: 20px;
+            font-weight: 600;
+            font-size: 20px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+          }
+          .text-tag {
+            color: #666;
+            margin: 10px 0;
+            display: flex;
+            align-items: center;
+          }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+          .text-desc {
+            color: #666;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            line-height: 1.3;
+          }
+        }
+        .info-data {
+          display: flex;
+          align-items: center;
+          color: #b4b4b4;
+          .data-view {
+            margin-right: 20px;
+          }
+        }
+      }
+      .item-right-img {
+        width: 100px;
+        height: 100px;
+        img {
+          width: 100px;
+          height: 100px;
+        }
+      }
+    }
+  }
 }
 </style>
